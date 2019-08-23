ca4576216e03af6bd0f15ea5a18fe776f27ab5d5
poornaui <poorna@ubsols.com>
2019-08-23 23:26:39 +0530

    angular token based authentication code

---
+++ ngApp/src/app/auth.service.ts
@@ -0,0 +1,34 @@
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  // tslint:disable-next-line:variable-name
+  private _registerUrl = 'http://localhost:3000/api/register';
+  private _loginUrl = 'http://localhost:3000/api/login';
+
+  constructor(private http: HttpClient, private _router: Router) { }
+
+  registerUser(user){
+    return this.http.post<any>(this._registerUrl, user)
+  }
+
+  loginUser(user){
+    return this.http.post<any>(this._loginUrl, user)
+  }
+
+  loggedIn(){
+    return !!localStorage.getItem('token')
+  }
+  logoutUser(){
+    localStorage.removeItem('token')
+    this._router.navigate(['/events'])
+  }
+  getToken(){
+    return localStorage.getItem('token')
+  }
+}
